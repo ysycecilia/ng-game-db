18b03d4be5fd36b6552975584626335aa221521f
ysycecilia <ysycecilia@gmail.com>
2021-09-16 14:50:52 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GaugeModule } from 'angular-gauge';
+import { MatIconModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    GaugeModule,
+    MatIconModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
